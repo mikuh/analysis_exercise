66ddc08de330db7ab114039f727a847b30a93017
fengyanglu <853934146@qq.com>
2017-08-18 23:45:40 +0800

    增加内容

--- 1.py
+++ 1.py
@@ -1,6 +1,22 @@
 import json
+from pandas import DataFrame, Series
+import pandas as pd
+import numpy as np
 
 with open('gov.txt', 'r') as f:
     records = [json.loads(line) for line in f]
 
-print(records[0])
+
+
+frame = DataFrame(records)
+
+print(frame['tz'][:10])
+
+# 将缺失字段填充为Unknown
+clean_tz = frame['tz'].fillna('Missing')
+clean_tz[clean_tz == '']='Unknow'
+
+# 按照值统计频率
+tz_counts = clean_tz.value_counts()
+# 展示直方图
+tz_counts[:10].plot(kind='barh', rot=0)
